927b27cfcff0891d4e4c8a54b0382784f1b46df3
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-10-29 16:12:50 -0400

    Added HTML file generated by Twine

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(__dirname + '/game.html')
   })
-
-app.get('/group/:groupId', (req, res) => {
-  res.send("Welcome Group " + req.params.groupId + "!")
-})
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
